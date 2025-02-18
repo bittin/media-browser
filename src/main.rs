1a246df7dd042f4e203d6a8f449e5252078a8a9c
fangornsrealm <progs@fangornsrealm.eu>
2025-02-18 05:26:19 +0100

    fix: set M_MMAP_THRESHOLD to lower memory usage significantly

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,11 @@
 // Copyright 2024 Alexander Schwarzkopf
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Prevents glibc from hoarding memory via memory fragmentation.
+    #[cfg(all(not(feature = "jemalloc"), target_env = "gnu"))]
+    unsafe {
+        libc::mallopt(libc::M_MMAP_THRESHOLD, 65536);
+    }
+
     media_browser::main()
 }
